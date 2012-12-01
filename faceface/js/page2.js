d39fe5a2170eb4712a992c7df9a16ebebf27db0c
Viktor Stanchev <me@viktorstanchev.com>
2012-12-01 10:18:42 -0800

    I CAN FACE

--- faceface/js/page2.js
+++ faceface/js/page2.js
@@ -26,9 +26,93 @@
             itemList: dataList
         };
     WinJS.Namespace.define("DataExample", publicMembers);
+    WinJS.UI.processAll().then(function () {
+        $('.test').get(0).onclick = function () {
+            //var buf = new Buffer($('.test').get(0).src.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+            var base64 = $('.test').get(0).src.replace(/^data:image\/\w+;base64,/, "");
+
+            var oMyFormData = new FormData();
+
+            var data = {
+                key: "81a894859ee60f798e47c1a837713bc0",
+                image: $('.test').get(0).src.replace(/^data:image\/\w+;base64,/, "")
+            };
+            for (var key in data) {
+                oMyFormData.append(key, data[key]);
+            }
+            //var params = "key=" + data.key + "&image=" + data.image;
+            // The file and uri variables are already set by this time. bt is a namespace shortcut 
 
 
-    $('.test').onclick = function () {
-        var buf = new Buffer(data.dataurl.replace(/^data:image\/\w+;base64,/, ""), 'base64');
-    }
+            /* var bt = Windows.Networking.BackgroundTransfer; 
+            var uploader = new bt.BackgroundUploader(); 
+            var contentParts = [];  
+            // Instead of sending multiple files (as in the original sample), we'll create those parts that 
+            // match the POST example for Flickr on http://www.flickr.com/services/api/upload.example.html 
+            var part;  
+            part = new bt.BackgroundTransferContentPart(); 
+            part.setHeader("Content-Disposition", "form-data; name=\"key\""); 
+            part.setText("81a894859ee60f798e47c1a837713bc0"); 
+            contentParts.push(part);
+            part = new bt.BackgroundTransferContentPart(); 
+            part.setHeader("Content-Disposition", "form-data; name=\"photo\"; filename=\"TwoFacing\""); 
+            part.setHeader("Content-Type", "image/jpeg"); 
+            part.setText(atob($('.test').get(0).src.replace(/^data:image\/\w+;base64,/, ""))); 
+            contentParts.push(part);
+            var upload;
+            var promise;
+            // Create a new upload operation specifying a boundary string. 
+            uploader.createUploadAsync( Windows.Foundation.Uri("http://api.imgur.com/2/upload.json"),
+                contentParts, "form-data", "-----------------------------7d44e178b0434")
+                .then(function (uploadOperation) {
+                    // Start the upload and persist the promise          
+                    upload = uploadOperation;
+                    promise = uploadOperation.startAsync().then(function () {
+                        console.log(JSON.stringify(arguments));
+                    }, function () {
+                        console.log('fail');
+                        console.log(JSON.stringify(arguments));
+                    }, function () {
+                        console.log('progress')
+                    });
+                });
+            */
+
+
+            WinJS.xhr({
+                url: "http://api.imgur.com/2/upload.json",
+                type: "post",
+                data: oMyFormData
+            }).then(function (xhr) {
+                var imgr_url = JSON.parse(xhr.response).upload.links.original;
+                var oMyFormData2 = new FormData();
+                var data = {
+                    access_token: App.auth.token,
+                    url: imgr_url
+                };
+                for (var key in data) {
+                    oMyFormData2.append(key, data[key]);
+                }
+                WinJS.xhr({
+                    url: "https://graph.facebook.com/me/photos",
+                    type: "post",
+                    data: oMyFormData2
+                }).then(function (test) {
+                    console.log(test.response);
+                }, function (xhr) {
+                    console.log("failed to upload")
+                    console.log(xhr.response);
+                }, function () {
+                    console.log("phase 2 spinner");
+                });
+            }, function (xhr) {
+                console.log("failed to upload")
+                console.log(xhr.response);
+            }, function () {
+                console.log("phase 1 spinner");
+            });
+        }
+    });
+    
+    
 })();
