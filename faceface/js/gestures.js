a58077728763e65924b4f76a150800eb454ff6cd
nelk <nelkishere@gmail.com>
2012-12-01 17:15:32 -0800

    Fixed release movement with mouse pointer.

--- faceface/js/gestures.js
+++ faceface/js/gestures.js
@@ -3,12 +3,13 @@
 Gestures = new (function () {
 
     this.lastPosition = {};
+    this.movingImage = false;
 
     this.init = function () {
         var canvas = Canvas.getCanvas();
         canvas.addEventListener("MSPointerDown", MSPointerDown, false);
         canvas.addEventListener("MSPointerMove", MSPointerMove, false);
-        //canvas.addEventListener("MSPointerUp", canvasHandler, false);
+        canvas.addEventListener("MSPointerUp", MSPointerUp, false);
         //canvas.addEventListener("MSPointerOver", canvasHandler, false);
         //canvas.addEventListener("MSPointerOut", canvasHandler, false);
         //canvas.addEventListener("MSPointerCancel", canvasHandler, false);
@@ -18,9 +19,17 @@ Gestures = new (function () {
     function MSPointerDown(evt) {
         var currentPosition = { x: evt.currentPoint.rawPosition.x, y: evt.currentPoint.rawPosition.y };
         Gestures.lastPosition = currentPosition;
+        Gestures.movingImage = true;
     }
 
-    function MSPointerMove (evt) {
+    function MSPointerUp(evt) {
+        Gestures.movingImage = false;
+    }
+
+    function MSPointerMove(evt) {
+        if (!Gestures.movingImage) {
+            return;
+        }
         var canvas = Canvas.getCanvas();
         canvas.msSetPointerCapture(evt.pointerId);
         var currentPosition = { x: evt.currentPoint.rawPosition.x, y: evt.currentPoint.rawPosition.y };
