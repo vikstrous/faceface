6278f0247037261f65b18e9579eec93b62cc87df
shalecraig <shalecraig@gmail.com>
2012-12-01 18:15:35 -0800

    page 2 n stuff

--- faceface/js/page2.js
+++ faceface/js/page2.js
@@ -20,31 +20,31 @@
             oMyFormData.append(key, data[key]);
         }
         //var params = "key=" + data.key + "&image=" + data.image;
-        // The file and uri variables are already set by this time. bt is a namespace shortcut 
+        // The file and uri variables are already set by this time. bt is a namespace shortcut
 
 
-        /* var bt = Windows.Networking.BackgroundTransfer; 
-        var uploader = new bt.BackgroundUploader(); 
-        var contentParts = [];  
-        // Instead of sending multiple files (as in the original sample), we'll create those parts that 
-        // match the POST example for Flickr on http://www.flickr.com/services/api/upload.example.html 
-        var part;  
-        part = new bt.BackgroundTransferContentPart(); 
-        part.setHeader("Content-Disposition", "form-data; name=\"key\""); 
-        part.setText("81a894859ee60f798e47c1a837713bc0"); 
+        /* var bt = Windows.Networking.BackgroundTransfer;
+        var uploader = new bt.BackgroundUploader();
+        var contentParts = [];
+        // Instead of sending multiple files (as in the original sample), we'll create those parts that
+        // match the POST example for Flickr on http://www.flickr.com/services/api/upload.example.html
+        var part;
+        part = new bt.BackgroundTransferContentPart();
+        part.setHeader("Content-Disposition", "form-data; name=\"key\"");
+        part.setText("81a894859ee60f798e47c1a837713bc0");
         contentParts.push(part);
-        part = new bt.BackgroundTransferContentPart(); 
-        part.setHeader("Content-Disposition", "form-data; name=\"photo\"; filename=\"TwoFacing\""); 
-        part.setHeader("Content-Type", "image/jpeg"); 
-        part.setText(atob($('.test').get(0).src.replace(/^data:image\/\w+;base64,/, ""))); 
+        part = new bt.BackgroundTransferContentPart();
+        part.setHeader("Content-Disposition", "form-data; name=\"photo\"; filename=\"TwoFacing\"");
+        part.setHeader("Content-Type", "image/jpeg");
+        part.setText(atob($('.test').get(0).src.replace(/^data:image\/\w+;base64,/, "")));
         contentParts.push(part);
         var upload;
         var promise;
-        // Create a new upload operation specifying a boundary string. 
+        // Create a new upload operation specifying a boundary string.
         uploader.createUploadAsync( Windows.Foundation.Uri("http://api.imgur.com/2/upload.json"),
             contentParts, "form-data", "-----------------------------7d44e178b0434")
             .then(function (uploadOperation) {
-                // Start the upload and persist the promise          
+                // Start the upload and persist the promise
                 upload = uploadOperation;
                 promise = uploadOperation.startAsync().then(function () {
                     console.log(JSON.stringify(arguments));
@@ -58,6 +58,18 @@
         */
 
 
+        $('.upring').removeClass('hidden');
+        $('#upload_button').addClass('hidden');
+        setTimeout(function () {
+            $('.upring').addClass('hidden');
+            $('#upload_success').removeClass('hidden');
+            $('#upload_button').addClass('hidden');
+            setTimeout(function () {
+                $('.upring').addClass('hidden');
+                $('#upload_success').addClass('hidden');
+                $('#upload_button').removeClass('hidden');
+            }, 2000);
+        }, 5000);
         WinJS.xhr({
             url: "http://api.imgur.com/2/upload.json",
             type: "post",
@@ -73,6 +85,7 @@
             for (var key in data) {
                 oMyFormData2.append(key, data[key]);
             }
+
             WinJS.xhr({
                 url: "https://graph.facebook.com/me/photos",
                 type: "post",
